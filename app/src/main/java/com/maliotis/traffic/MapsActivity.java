34fdce2b458951b2957818383d356faa3c7a807f
SoKoS <komi1997@gmail.com>
2018-06-11 16:10:14 +0300

    Search Bar and Temp Icon

--- app/src/main/java/com/maliotis/traffic/MapsActivity.java
+++ app/src/main/java/com/maliotis/traffic/MapsActivity.java
@@ -13,6 +13,10 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.location.Address;
+import android.location.Geocoder;
+import android.view.View;
+import android.widget.EditText;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -28,6 +32,7 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.JointType;
 import com.google.android.gms.maps.model.RoundCap;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -191,6 +196,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         return ret;
 
+    }
+
+    // search bar code
+    public void onMapSearch(View view) {
+        EditText locationSearch = (EditText) findViewById(R.id.editText);
+        String location = locationSearch.getText().toString();
+        List<Address>addressList = null;
+
+        if (location != null || !location.equals("")) {
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList = geocoder.getFromLocationName(location, 1);
+
+            } catch (IOException e) {
+                // TODO: Here we need to handle the exception when the search doesn't exist
+                e.printStackTrace();
+            }
+            Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+            // make the camera go to the searched place
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        }
     }
 
 
