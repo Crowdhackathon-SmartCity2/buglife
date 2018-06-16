af268edab5f01732dbca573618afa272f12c918b
SoKoS <komi1997@gmail.com>
2018-06-16 23:41:39 +0300

    Auto Rotate

--- app/src/main/java/com/maliotis/traffic/MapsActivity.java
+++ app/src/main/java/com/maliotis/traffic/MapsActivity.java
@@ -40,6 +40,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.JointType;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
@@ -153,7 +154,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
         mCurrLocationMarker = mMap.addMarker(markerOptions);
 
-        //move map camera
+        //move map camera and rotate
+        updateCameraBearing(location.getBearing());
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoom));
 
     }
@@ -326,6 +328,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addPolyline(lineOptions);
     }
 
+    private void updateCameraBearing(float bearing) {
+        if ( mMap == null) return;
+        CameraPosition camPos = CameraPosition
+                .builder(
+                        mMap.getCameraPosition() // current Camera
+                )
+                .bearing(bearing)
+                .build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(camPos));
+    }
 
     public static Bitmap getBitmapFromVectorDrawable(Context context, int drawableId) {
         Drawable drawable = ContextCompat.getDrawable(context, drawableId);
