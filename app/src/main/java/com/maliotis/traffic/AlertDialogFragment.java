066191bed6ef72d076d66c5b8ec862bf63a0117e
xiji <confused---mind@hotmail.com>
2018-06-18 19:23:14 +0300

    AAAAAAAAAAAAAAAAAAAAAAAA

---
+++ app/src/main/java/com/maliotis/traffic/AlertDialogFragment.java
@@ -0,0 +1,34 @@
+package com.maliotis.traffic;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+
+public class AlertDialogFragment extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("ERROR");
+        builder.setMessage("Somehing went rong!");
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dismiss();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dismiss();
+            }
+        });
+        return builder.create();
+    }
+}
