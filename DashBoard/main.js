81ff175e09e9d250c2395817038745faeb3cc21f
SoKoS <komi1997@gmail.com>
2018-06-29 23:34:53 +0300

    Refactoring

--- DashBoard/main.js
+++ DashBoard/main.js
@@ -2,12 +2,9 @@ var map;
 
 var messagesRef = new Firebase("https://ne-7aac7.firebaseio.com/");
 
-function writeUserData() {
-    messagesRef.push({ "TEST123TEST": 1555151 });
-}
 
 function initMap() {
-    var myLatlng = { lat: -25.363, lng: 131.044 };
+    var myLatlng = { lat: 37.946966, lng: 23.723573 };
 
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 4,
@@ -21,6 +18,12 @@ function initMap() {
     });
 
     marker.addListener('click', function () {
-        writeUserData();
+        for (i  = 0; i < 10; i++) {
+            addWaypoint("123a321b32a21c543a34b23a513"+i, 5+i);
+        }
     });
+}
+
+function addWaypoint(waypoint, percentDisabled) {
+    messagesRef.child("Waypoints").update({ [waypoint]:percentDisabled });
 }
