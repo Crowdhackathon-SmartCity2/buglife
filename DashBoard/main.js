1910956825780861159fcde4c8ca12b00e6d154a
SoKoS <komi1997@gmail.com>
2018-06-29 22:32:25 +0300

    sending to Firebase with marker click event

--- DashBoard/main.js
+++ DashBoard/main.js
@@ -1,27 +1,10 @@
-require("firebase/database");
-
-
 var map;
 
-var firebase = new Firebase('https://ne-7aac7.firebaseio.com/');
+var messagesRef = new Firebase("https://ne-7aac7.firebaseio.com/");
 
-//data to send to firebase
-var data = {
-    sender: null,
-    timestamp: null,
-    lat: null,
-    lng: null
-};
-
-// Firebase initialaization
-var config = {
-    apiKey: "AIzaSyAdMZd6mAYpeiupJbzSsvRsqaI_EAiNd4E",
-    authDomain: "ne-7aac7.firebaseapp.com",
-    databaseURL: "https://ne-7aac7.firebaseio.com",
-    projectId: "ne-7aac7",
-    storageBucket: "ne-7aac7.appspot.com"
-};
-firebase.initializeApp(config);
+function writeUserData() {
+    messagesRef.push({ "TEST123TEST": 1555151 });
+}
 
 function initMap() {
     var myLatlng = { lat: -25.363, lng: 131.044 };
@@ -38,7 +21,6 @@ function initMap() {
     });
 
     marker.addListener('click', function () {
-        map.setZoom(8);
-        map.setCenter(marker.getPosition());
+        writeUserData();
     });
 }
